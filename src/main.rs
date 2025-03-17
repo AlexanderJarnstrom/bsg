947e30c35a83737287cbebcbc3c9d64b4ab75a05
Morgan <mnordberg01@gmail.com>
2025-03-17 17:36:21 +0100

    Basic Raylib window

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use raylib::ffi::TraceLogLevel::LOG_NONE;
+use raylib::prelude::*;
+
+const SCREEN_WIDTH: i32 = 64;
+const SCREEN_HEIGHT: i32 = 32;
+
 fn main() {
-    println!("Hello, world!");
+    set_trace_log(LOG_NONE);
+    
+    let (mut rl, thread) = raylib::init().size(640, 480).title("AWESOME GAME").build();
+
+    while !rl.window_should_close() {
+        let mut d = rl.begin_drawing(&thread);
+
+        d.clear_background(Color::BLACK);
+    }
 }
